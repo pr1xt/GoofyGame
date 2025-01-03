2106bd7009cc77b24a36717382db0084ff2e3fdd
pr1xt <timurkn2006@gmail.com>
2025-01-03 14:32:52 +0100

    minor changes

--- Assets/scripts/PlateBehavior.cs
+++ Assets/scripts/PlateBehavior.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 public class PlateBehavior : MonoBehaviour
 {
 
+    public GameObject bulletPrefab;
     public float speed = 1f;
     
     public float LeftBorder = -3f;
-    
+    private float bulletSpeed = 10f;
     public float RightBorder = 3f;
     Vector3 targetPosition = new Vector3(-3f,0.5f,6);
     
@@ -26,5 +27,25 @@ public class PlateBehavior : MonoBehaviour
             Debug.Log("Right border touched");
         }
         transform.position = Vector3.MoveTowards(transform.position, targetPosition, speed * Time.deltaTime);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Connect");
+        // Optional: Handle collision with enemies or objects
+        if (other.CompareTag("bullet"))
+        {
+            float randomNumber = UnityEngine.Random.Range(-20f, 20f) / 10f;
+
+            Vector3 firepoint = new Vector3(transform.position.x+randomNumber,transform.position.y+randomNumber,7);
+    
+            GameObject bullet = Instantiate(bulletPrefab, firepoint, transform.rotation);
+            
+            
+            // Rigidbody rb = bullet.GetComponent<Rigidbody>();
+            // if (rb != null)
+            // {
+            //     rb.linearVelocity = firePoint.forward * bulletSpeed;
+            // }
+        }
     }
 }
